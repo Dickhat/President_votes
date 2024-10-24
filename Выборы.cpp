bf1e0df8b7d270d26fc1c5c98be902b43ab45a4f
Eldar Bagirov <35378787+eldarbr@users.noreply.github.com>
2024-10-24 20:45:59 +0300

    fix: apply code formatting (#1)

--- Выборы.cpp
+++ Выборы.cpp
@@ -1,66 +1,64 @@
 ﻿#include <iostream>
 #include <semaphore>
 #include <thread>
+
 #include "windows.h"
 
 // Семафор для голосов первого и второго кандидата
-std::binary_semaphore sem1{ 1 }, sem2{ 1 };
+std::binary_semaphore sem1{1}, sem2{1};
 
-void first_candidate(int * votes_for_first)
-{
-    // Увеличение числа голосов
-    while (true)
-    {
-        sem1.acquire();
-        (*votes_for_first)++;
-        sem1.release();
+void first_candidate(int* votes_for_first) {
+  // Увеличение числа голосов
+  while (true) {
+    sem1.acquire();
+    (*votes_for_first)++;
+    sem1.release();
 
-        Sleep(std::rand()/1000);
-    }
+    Sleep(std::rand() / 1000);
+  }
 }
 
-void second_candidate(int* votes_for_second)
-{
-    // Увеличение числа голосов
-    while (true)
-    {
-        sem2.acquire();
-        (*votes_for_second)++;
-        sem2.release();
+void second_candidate(int* votes_for_second) {
+  // Увеличение числа голосов
+  while (true) {
+    sem2.acquire();
+    (*votes_for_second)++;
+    sem2.release();
 
-        Sleep(std::rand() / 500);
-    }
+    Sleep(std::rand() / 500);
+  }
 }
 
-int main()
-{
-    setlocale(LC_ALL, "RUSSIAN");
+int main() {
+  setlocale(LC_ALL, "RUSSIAN");
 
-    int total_count = 0;
-    int votes_for_first = 0;
-    int votes_for_second = 0;
+  int total_count = 0;
+  int votes_for_first = 0;
+  int votes_for_second = 0;
 
-    // Запуск подсчета подписей
-    std::thread thrWorker1(first_candidate, &votes_for_first);
-    std::thread thrWorker2(second_candidate, &votes_for_second);
+  // Запуск подсчета подписей
+  std::thread thrWorker1(first_candidate, &votes_for_first);
+  std::thread thrWorker2(second_candidate, &votes_for_second);
 
-    Sleep(1000);
+  Sleep(1000);
 
-    // Обновление процентажа голосов
-    while (true)
-    {
-        sem1.acquire();
-        sem2.acquire();
+  // Обновление процентажа голосов
+  while (true) {
+    sem1.acquire();
+    sem2.acquire();
 
-        total_count = votes_for_first + votes_for_second;
-        std::cout << "\n\n Общее число голосов: " << total_count << std::endl;
-        std::cout << "Голосов за первого кандидата: " << votes_for_first / float(total_count) * 100 << "%(" << votes_for_first << ")\n";
-        std::cout << "Голосов за второго кандидата: " << votes_for_second / float(total_count) * 100 << "%(" << votes_for_second << ")\n";
+    total_count = votes_for_first + votes_for_second;
+    std::cout << "\n\n Общее число голосов: " << total_count << std::endl;
+    std::cout << "Голосов за первого кандидата: "
+              << votes_for_first / float(total_count) * 100 << "%("
+              << votes_for_first << ")\n";
+    std::cout << "Голосов за второго кандидата: "
+              << votes_for_second / float(total_count) * 100 << "%("
+              << votes_for_second << ")\n";
 
-        sem1.release();
-        sem2.release();
+    sem1.release();
+    sem2.release();
 
-        Sleep(1500);
-    }
-
+    Sleep(1500);
+  }
 }
